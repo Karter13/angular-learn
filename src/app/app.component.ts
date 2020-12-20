99f8230982b8e913a343396914d517c886aacdca
Karter13 <KarMiH1975@mail.ru>
2020-12-20 13:03:59 +0300

    created learn project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+      <h1>{{title}}</h1>
+      <h2>My favorite hero is: {{myHero}}</h2>
+    <p>Heroes:</p>
+    <ul>
+        <li *ngFor="let hero of heroes">
+            {{hero}}
+        </li>
+    </ul>
+  `,
 })
 export class AppComponent {
-  title = 'angular-learn';
+  title = 'Tour of Heroes';
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  myHero = this.heroes[0];
 }
