7ef27cf8296228f59e4e3c1e27f8bb4a7061d1a3
Karter13 <KarMiH1975@mail.ru>
2020-12-20 13:41:03 +0300

    *ngFor *ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Hero} from './hero';
 
 @Component({
   selector: 'app-root',
   template: `
       <h1>{{title}}</h1>
-      <h2>My favorite hero is: {{myHero}}</h2>
-    <p>Heroes:</p>
-    <ul>
-        <li *ngFor="let hero of heroes">
-            {{hero}}
-        </li>
-    </ul>
+      <h2>My favorite hero is: {{myHero.name}}</h2>
+      <p>Heroes:</p>
+      <ul>
+          <li *ngFor="let hero of heroes">
+              {{hero.name}}
+          </li>
+      </ul>
+    <p *ngIf="heroes.length > 3">There are many heroes!</p>
   `,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Tour of Heroes';
-  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  heroes = [
+    new Hero(1, 'Windstorm'),
+    new Hero(13, 'Bombasto'),
+    new Hero(15, 'Magneta'),
+    new Hero(20, 'Tornado'),
+  ];
   myHero = this.heroes[0];
+
+  ngOnInit(): void {
+    console.log(this.heroes);
+  }
+
 }
